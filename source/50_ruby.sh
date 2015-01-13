0786e63022e5ddb827f1ceffa672829231322800
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2015-01-13 10:12:41 -0800

    Comment out Ruby-specific source file until/unless I need it

--- source/50_ruby.sh
+++ source/50_ruby.sh
@@ -1,8 +1,8 @@
-export PATH
+#export PATH
 
 # rbenv init.
-PATH="$(path_remove $DOTFILES/vendor/rbenv/bin):$DOTFILES/vendor/rbenv/bin"
+#PATH="$(path_remove $DOTFILES/vendor/rbenv/bin):$DOTFILES/vendor/rbenv/bin"
 
-if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
-  eval "$(rbenv init -)"
-fi
+#if [[ "$(type -P rbenv)" && ! "$(type -t _rbenv)" ]]; then
+#  eval "$(rbenv init -)"
+#fi
