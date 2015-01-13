c0f6f613a9ac99513df3ee242bee89689d76b89f
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2015-01-13 09:22:10 -0800

    Comment out OS X init code that I don't think I want, at least not now

--- init/10_osx_xcode.sh
+++ init/10_osx_xcode.sh
@@ -4,6 +4,11 @@ is_osx || return 1
 # Some tools look for XCode, even though they don't need it.
 # https://github.com/joyent/node/issues/3681
 # https://github.com/mxcl/homebrew/issues/10245
-if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
-  sudo xcode-select -switch /usr/bin
-fi
+
+# jafinger TBD: I think that I already have Xcode set up reasonably on
+# all my Macs, and I am not yet sure what these commands might do to
+# change that.  Comment it out for now.
+
+#if [[ ! -d "$('xcode-select' -print-path 2>/dev/null)" ]]; then
+#  sudo xcode-select -switch /usr/bin
+#fi
--- init/50_osx_fonts.sh
+++ init/50_osx_fonts.sh
@@ -1,17 +1,20 @@
 # OSX-only stuff. Abort if not OSX.
 is_osx || return 1
 
-# Copy fonts
-{
-  pushd $DOTFILES/conf/osx/fonts/; setdiffA=(*); popd
-  pushd ~/Library/Fonts/; setdiffB=(*); popd
-  setdiff
-} >/dev/null
+# jafinger: I don't know if I want these fonts or not.  Leave them out
+# for now.
 
-if (( ${#setdiffC[@]} > 0 )); then
-  e_header "Copying fonts (${#setdiffC[@]})"
-  for f in "${setdiffC[@]}"; do
-    e_arrow "$f"
-    cp "$DOTFILES/conf/osx/fonts/$f" ~/Library/Fonts/
-  done
-fi
+# Copy fonts
+#{
+#  pushd $DOTFILES/conf/osx/fonts/; setdiffA=(*); popd
+#  pushd ~/Library/Fonts/; setdiffB=(*); popd
+#  setdiff
+#} >/dev/null
+#
+#if (( ${#setdiffC[@]} > 0 )); then
+#  e_header "Copying fonts (${#setdiffC[@]})"
+#  for f in "${setdiffC[@]}"; do
+#    e_arrow "$f"
+#    cp "$DOTFILES/conf/osx/fonts/$f" ~/Library/Fonts/
+#  done
+#fi
