60ba8bcc6f0442f83ba557116754139bb247267f
Andy Fingerhut <EMAIL>
2017-03-29 22:41:52 -0700

    Add /opt/local/bin early in PATH on OSX machines

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -2,8 +2,11 @@
 is_osx || return 1
 
 # APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
-PATH="/usr/local/bin:$(path_remove /usr/local/bin)"
-export PATH
+export PATH="/usr/local/bin:$(path_remove /usr/local/bin)"
+if [ -d /opt/local/bin ]
+then
+    export PATH="/opt/local/bin:$(path_remove /opt/local/bin)"
+fi
 
 # Trim new lines and copy to clipboard
 alias c="tr -d '\n' | pbcopy"
