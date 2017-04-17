a80d707ebd58c1f70f0a2a91810596b8f75a07fc
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-04-17 14:16:12 -0700

    Ubuntu 16.04 version of grep says GREP_OPTIONS is deprecated
    so changing it to an alias instead.

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -5,7 +5,7 @@ shopt -s nocaseglob
 # update the values of LINES and COLUMNS.
 shopt -s checkwinsize
 
-export GREP_OPTIONS='--color=auto'
+alias grep='grep --color=auto'
 
 alias m='less'
 # TBD: Is this useful for anything?
