915168b0641a20bd2ca567f19b811ab20113e26c
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-04-27 21:15:23 -0700

    Add notes on a successful rebase of a Github PR

--- bin/git-rebase.sh
+++ bin/git-rebase.sh
@@ -1,5 +1,29 @@
 #! /bin/bash
 
+######################################################################
+# Advice from Chris Dodd on rebasing a Github PR, which I followed for
+# one PR I had on p4lang/p4c repo, from my Github fork where my
+# changes were on my fork's master branch, and it worked!
+#
+# Generally the way to deal with this (upstream changes to master you
+# want in your working branch that you've already made a pull request
+# for) is to use
+#
+#     git pull --rebase upstream master
+#
+# (or possibly origin master if your local repo is clone of
+# p4c.org/p4c with your fork added as an upstream, rather than a clone
+# of your fork, with p4c.org/p4c added as upstream)
+#
+# Followed by
+#
+#    git push -f
+#
+# to update the pull request in place.
+#
+######################################################################
+
+
 # From Calin Cascaval
 
 #MY_GITHUB_FORK_NAME="cascaval"
