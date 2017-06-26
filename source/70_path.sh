6764dc777ba05dd8a9f2cb02c27fc8e72144a580
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-06-26 07:40:13 -0700

    Add conf/x-app-defaults directory and XAPPRESDIR env var setting

--- source/70_path.sh
+++ source/70_path.sh
@@ -14,3 +14,5 @@ prepend_to_path_if_exists "$HOME/bin"
 prepend_to_path_if_exists "$HOME/bin/${MY_OS}"
 prepend_to_path_if_exists "$HOME/bin/${MY_OS}-${MY_MACHINE}"
 prepend_to_path_if_exists "$HOME/bin/atp"
+
+export XAPPLRESDIR="${DOTFILES}/conf/x-app-defaults"
