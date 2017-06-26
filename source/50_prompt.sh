05be9b0c014ba53eed84e162a7a09e97d40b5a80
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-06-26 00:49:38 -0700

    Put most of first prompt line in 'inverse' color

--- source/50_prompt.sh
+++ source/50_prompt.sh
@@ -27,6 +27,7 @@ if [[ ! "${prompt_colors[@]}" ]]; then
     "36" # information color
     "37" # bracket color
     "31" # error color
+    "7"  # inverse
   )
 
   if [[ "$SSH_TTY" ]]; then
@@ -131,7 +132,8 @@ function prompt_command() {
   # misc: [cmd#:hist#]
   # PS1="$PS1$c1[$c0#\#$c1:$c0!\!$c1]$c9"
   # path: [user@host:path]
-  PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
+  #PS1="$PS1$c1[$c0\u$c1@$c0\h$c1:$c0\w$c1]$c9"
+  PS1="$PS1$c3[\u@\h:\w]$c9"
   PS1="$PS1\n"
   # date: [HH:MM:SS]
   PS1="$PS1$c1[$c0$(date +"%H$c1:$c0%M$c1:$c0%S")$c1]$c9"
