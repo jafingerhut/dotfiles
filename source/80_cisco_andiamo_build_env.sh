9405ca507d449d9cfc417d3cfe72c8b6060f776f
Andy Fingerhut <jafinger@cisco.com>
2017-06-26 18:23:10 -0700

    Add a couple of bash setup scripts for Cisco paths, etc.

---
+++ source/80_cisco_andiamo_build_env.sh
@@ -0,0 +1,5 @@
+if [ -f /auto/andatc/independent/shellrc-files/current/rc/.bashrc.build ]
+then
+    #unset ATC_VegasBuildEnv
+    alias andatc='source /auto/andatc/independent/shellrc-files/current/rc/.bashrc.build'
+fi
---
+++ source/80_cisco_paths.sh
@@ -0,0 +1,13 @@
+prepend_to_path_if_exists () {
+    local dir=$1
+    if [ -d "${dir}" ]
+    then
+        export PATH="${dir}:$(path_remove ${dir})"
+    fi
+}
+
+# Cisco paths
+prepend_to_path_if_exists "/users/khaltore/lab/reserve"
+prepend_to_path_if_exists "/auto/netsift/tools/i686/swig-1.3.39/bin"
+# /usr/cisco/bin contains git and many other things
+prepend_to_path_if_exists "/usr/cisco/bin"
