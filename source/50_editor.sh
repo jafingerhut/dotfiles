d8685843f068cf410ff2627b4b2b87dca509adc6
Andy Fingerhut <jafinger@cisco.com>
2017-06-26 18:21:16 -0700

    Don't create so many shell variables while setting up Emacs binary

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,14 +1,24 @@
 # Editing
 
-# On my Macs, I like to use the MacPorts-installed version of Emacs
-# over the version that comes with OS X by default.
-export MACPORTS_EMACS="/opt/local/bin/emacs"
-export EMACS="emacs"
-if [ -f "${MACPORTS_EMACS}" ]; then
-    export EMACS="${MACPORTS_EMACS}"
+alias vi=vim
+
+if [ -x "/opt/local/bin/emacs" ]
+then
+    # On my Macs, I like to use the MacPorts-installed version of
+    # Emacs over the version that comes with OS X by default, which is
+    # still version 21.x originally released in 2007, even on the
+    # latest Mac OS Sierra!
+    EMACS="/opt/local/bin/emacs"
+elif [ -x "/router/bin/emacs-24.5" ]
+then
+    # Among machines I commonly use, /router/bin/emacs-24.5 only
+    # exists on Cisco Enterprise Linux machines.
+    EMACS="/router/bin/emacs-24.5"
+else
+    EMACS="emacs"
 fi
 
 export EDITOR="${EMACS}"
 export VISUAL="${EDITOR}"
 alias e="${EDITOR}"
-alias vi=vim
+alias emacs="${EDITOR}"
