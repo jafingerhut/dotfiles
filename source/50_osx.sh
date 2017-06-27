3635ee6490a8fcb0bc4ba69ccea2f08337550abd
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-06-26 22:23:06 -0700

    Add /usr/share/man to MANPATH

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -6,7 +6,6 @@ prepend_to_path_if_exists "/usr/local/bin"
 prepend_to_path_if_exists "/opt/local/bin"
 prepend_to_path_if_exists "/opt/local/sbin"
 
-prepend_to_manpath_if_exists "/opt/local/share/man"
 # For some reason, these are not there by default:
 prepend_to_manpath_if_exists "/Developer/usr/share/man"
 prepend_to_manpath_if_exists "/Developer/usr/X11/share/man"
@@ -14,6 +13,7 @@ prepend_to_manpath_if_exists "/Developer/usr/llvm-gcc-4.2/share/man"
 
 prepend_to_manpath_if_exists "/opt/local/share/man"
 prepend_to_manpath_if_exists "$HOME/share/man"
+prepend_to_manpath_if_exists "/usr/share/man"
 
 # Trim new lines and copy to clipboard
 alias c="tr -d '\n' | pbcopy"
