b9f081ed834b613eb6cabc8cb281e001884c1884
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2017-11-04 10:03:06 -0700

    Add small script to git clone p4pktgen repo from bitbucket.org
    I don't yet remember the repository URL as readily as I remember the
    ones on Github.

---
+++ bin/get-p4pktgen.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# This is my fork of the p4pktgen repository
+git clone git@bitbucket.org:jafingerhut/p4pktgen.git
+
+# This is the main repository
+#git clone git@bitbucket.org:p4pktgen/p4pktgen.git
