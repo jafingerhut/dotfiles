5925c074a964a587c1def0a7edd9beffbf3b45fe
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-02-14 08:04:15 -0800

    Add script to change mate-terminal settings to my liking

---
+++ bin/change-mate-terminal-settings.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# $ dconf list /org/mate/terminal/profiles/default/
+# background-color
+# palette
+# bold-color
+# foreground-color
+# visible-name
+
+KEYPATH="/org/mate/terminal/profiles/default/"
+
+show_settings() {
+    for j in `dconf list ${KEYPATH}`
+    do
+        echo "$j - `dconf read ${KEYPATH}${j}`"
+    done
+}
+
+echo "Settings before attempted changes:"
+show_settings
+dconf write ${KEYPATH}use-theme-colors false
+dconf write ${KEYPATH}background-color "'#2d2d09092222'"
+dconf write ${KEYPATH}foreground-color "'#ffffffffffff'"
+
+echo ""
+echo "Settings after attempted changes:"
+show_settings
