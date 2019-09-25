0cd8d5dd4324fa803ccdd2eff0b00482c6c88e36
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2019-09-25 14:27:21 -0700

    Make ediff work using my custom EMACS env variable

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -7,21 +7,21 @@ then
     # On a new Mac of mine, I am going to give it a whirl to try using
     # GNU Emacs for Mac OS X downloaded from
     # https://emacsformacosx.com
-    EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
+    export EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
 elif [ -x "/opt/local/bin/emacs" ]
 then
     # On my Macs, I like to use the MacPorts-installed version of
     # Emacs over the version that comes with OS X by default, which is
     # still version 21.x originally released in 2007, even on the
     # latest Mac OS Sierra!
-    EMACS="/opt/local/bin/emacs"
+    export EMACS="/opt/local/bin/emacs"
 elif [ -x "/router/bin/emacs-24.5" ]
 then
     # Among machines I commonly use, /router/bin/emacs-24.5 only
     # exists on Cisco Enterprise Linux machines.
-    EMACS="/router/bin/emacs-24.5"
+    export EMACS="/router/bin/emacs-24.5"
 else
-    EMACS="emacs"
+    export EMACS="emacs"
 fi
 
 export EDITOR="${EMACS}"
