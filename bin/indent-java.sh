1cd884d4db51c48cfc91deffb9ad35dcce37a13b
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-11-16 21:01:27 -0500

    Changes to indent-java.sh script that work with GNU sed on Linux, at least

--- bin/indent-java.sh
+++ bin/indent-java.sh
@@ -18,9 +18,19 @@ for j in $*
 do
     echo $j
     /bin/cp $j $j.orig
-    astyle --style=java --pad-first-paren-out $j
+    # --pad-first-paren-out seems to add space before a left paren
+    #that surrounds the parameters of a method call, too, but I do not
+    #want that.
+    #astyle --style=java --pad-first-paren-out $j
+    astyle --style=java $j
     set -x
     /bin/cp $j ${TMP_FNAME}
-    sed 's/\(if|switch|for|while\)(/\1 \(/g' ${TMP_FNAME} > $j
+    # I wish I knew how to make astyle force a space between
+    # if/switch/while/for and the following left parenthesis, but not
+    # between a method call and the following left parenthesis.  Until
+    # then, this is fairly close.  Unfortunately it will muck with the
+    # contents of any Java strings that contain this pattern, too, but
+    # that is rare in most Java program.
+    sed 's/\(if\|switch\|for\|while\)(/\1 (/g' ${TMP_FNAME} > $j
 done
 
