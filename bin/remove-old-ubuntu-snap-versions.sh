3ed76a53ebbd102e7c1a847d6e7b542c1bb5821f
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2021-12-24 09:01:59 -0800

    Update script to also delete the snap cache files

--- bin/remove-old-ubuntu-snap-versions.sh
+++ bin/remove-old-ubuntu-snap-versions.sh
@@ -6,9 +6,18 @@
 # I found this script here:
 # https://www.linuxuprising.com/2019/04/how-to-remove-old-snap-versions-to-free.html
 
+# In this discussion I found that it should be safe to delete the snap
+# cache directory:
+# https://askubuntu.com/questions/1075050/how-to-remove-uninstalled-snaps-from-cache
+
 set -eu
 
+BEFORE=`df -BM .`
 LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
         sudo snap remove "$snapname" --revision="$revision"
     done
+sudo rm -rf /var/lib/snapd/cache/*
+AFTER=`df -BM .`
+echo "Before: $BEFORE"
+echo "After:  $AFTER"
