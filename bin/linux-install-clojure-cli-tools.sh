27485a051d02e37198616d07a9341a93c94d069f
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2022-04-17 12:59:51 -0400

    Minor addition to previous commit

--- bin/linux-install-clojure-cli-tools.sh
+++ bin/linux-install-clojure-cli-tools.sh
@@ -5,6 +5,7 @@ FNAME="linux-install-1.11.1.1105.sh"
 echo "Version of Linux Clojure CLI tools last checked on"
 echo "https://clojure.org/guides/getting_started on 2022-Apr-17"
 echo "Version: ${FNAME}"
+echo ""
 
 set -x
 curl -O https://download.clojure.org/install/${FNAME}
