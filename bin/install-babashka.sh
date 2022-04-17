00316f37ab735056c294287a961c642a56399441
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2022-04-17 13:06:25 -0400

    Add tiny bash scripts to install Homebrew and Babashka

---
+++ bin/install-babashka.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "Version of Babashka install instructions last checked on"
+echo "https://github.com/babashka/babashka#installation on 2022-Apr-17"
+echo ""
+
+set -x
+brew install borkdude/brew/babashka
---
+++ bin/install-homebrew.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+echo "Version of Linux Homebrew install instructions last checked on"
+echo "https://brew.sh on 2022-Apr-17"
+echo ""
+
+set -x
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
