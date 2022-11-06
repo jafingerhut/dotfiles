86f5c54be36bf62dda6078960b0cf30bdba4166a
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2022-11-06 13:44:42 -0500

    Make command for removing files from snap cache work correctly

--- bin/remove-old-ubuntu-snap-versions.sh
+++ bin/remove-old-ubuntu-snap-versions.sh
@@ -17,7 +17,9 @@ LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
     while read snapname revision; do
         sudo snap remove "$snapname" --revision="$revision"
     done
-sudo rm -rf /var/lib/snapd/cache/*
+# askubuntu.com Q&A where I found the following command:
+# https://askubuntu.com/questions/1075050/how-to-remove-uninstalled-snaps-from-cache
+sudo sh -c 'rm -rf /var/lib/snapd/cache/*'
 sudo apt clean
 sudo apt autoremove
 sudo apt clean
