fcc699a457cfc91d823c4bcc8766b03db47c387a
jfingerh <john.andy.fingerhut@intel.com>
2023-04-12 11:55:23 -0400

    Update ssh-agent-setup-<github-userid>.bash scripts so that they update the ~/.gitconfig file as desired for that github user id.

--- bin/ssh-agent-setup-jafingerhut.bash
+++ bin/ssh-agent-setup-jafingerhut.bash
@@ -1,3 +1,7 @@
 ssh-agent -s >| ~/source-jafingerhut
+echo 'git config --global user.name "Andy Fingerhut"' >> ~/source-jafingerhut
+echo 'git config --global user.email "andy_fingerhut@alum.wustl.edu"' >> ~/source-jafingerhut
+echo 'git config --global credential.helper cache' >> ~/source-jafingerhut
+echo 'echo "Updated ~/.gitconfig for user id jafingerhut"' >> ~/source-jafingerhut
 source ~/source-jafingerhut
 ssh-add ~/.ssh/id_ed25519
--- bin/ssh-agent-setup-jfingerh.bash
+++ bin/ssh-agent-setup-jfingerh.bash
@@ -1,3 +1,7 @@
 ssh-agent -s >| ~/source-jfingerh
+echo 'git config --global user.name "jfingerh"' >> ~/source-jfingerh
+echo 'git config --global user.email "john.andy.fingerhut@intel.com"' >> ~/source-jfingerh
+echo 'git config --global credential.helper cache' >> ~/source-jfingerh
+echo 'echo "Updated ~/.gitconfig for user id jfingerh"' >> ~/source-jfingerh
 source ~/source-jfingerh
 ssh-add ~/.ssh/id_rsa
