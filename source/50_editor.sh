6538f574d03b4f6a60f58e0b0968a40ef820da19
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-06-06 14:21:26 -0400

    Update some old years-old comments, and comment out some code ... ... that seems unlikely to be used again.

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -10,22 +10,14 @@ fi
 
 if [ -x "/Applications/Emacs.app/Contents/MacOS/Emacs" ]
 then
-    # On a new Mac of mine, I am going to give it a whirl to try using
-    # GNU Emacs for Mac OS X downloaded from
-    # https://emacsformacosx.com
+    # I often use GNU Emacs for Mac OS X downloaded from
+    # https://emacsformacosx.com on my macOS systems.
     export EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
 elif [ -x "/opt/local/bin/emacs" ]
 then
-    # On my Macs, I like to use the MacPorts-installed version of
-    # Emacs over the version that comes with OS X by default, which is
-    # still version 21.x originally released in 2007, even on the
-    # latest Mac OS Sierra!
+    # Several years ago now, I often used GNU Emacs as installed by
+    # MacPorts.
     export EMACS="/opt/local/bin/emacs"
-elif [ -x "/router/bin/emacs-24.5" ]
-then
-    # Among machines I commonly use, /router/bin/emacs-24.5 only
-    # exists on Cisco Enterprise Linux machines.
-    export EMACS="/router/bin/emacs-24.5"
 else
     export EMACS=`which emacs`
 fi
--- source/80_cisco_paths.sh
+++ source/80_cisco_paths.sh
@@ -1,5 +1,5 @@
 # Cisco paths
-prepend_to_path_if_exists "/users/khaltore/lab/reserve"
-prepend_to_path_if_exists "/auto/netsift/tools/i686/swig-1.3.39/bin"
+#prepend_to_path_if_exists "/users/khaltore/lab/reserve"
+#prepend_to_path_if_exists "/auto/netsift/tools/i686/swig-1.3.39/bin"
 # /usr/cisco/bin contains git and many other things
-prepend_to_path_if_exists "/usr/cisco/bin"
+#prepend_to_path_if_exists "/usr/cisco/bin"
