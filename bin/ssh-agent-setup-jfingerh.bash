fc472ffb8ee46ee77cb449acecacb16c1d41fc44
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-06-20 17:38:25 -0400

    Remove obsolete file

--- bin/ssh-agent-setup-jfingerh.bash
+++
@@ -1,7 +0,0 @@
-ssh-agent -s >| ~/source-jfingerh
-echo 'git config --global user.name "jfingerh"' >> ~/source-jfingerh
-echo 'git config --global user.email "john.andy.fingerhut@intel.com"' >> ~/source-jfingerh
-echo 'git config --global credential.helper cache' >> ~/source-jfingerh
-echo 'echo "Updated ~/.gitconfig for user id jfingerh"' >> ~/source-jfingerh
-source ~/source-jfingerh
-ssh-add ~/.ssh/id_rsa
