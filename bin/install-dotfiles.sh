a7f803c2f17426a603a7002f7c18c78711e59799
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2024-07-14 15:41:01 -0400

    Abort with a message on how to change shell to bash if zsh is current shell

--- bin/install-dotfiles.sh
+++ bin/install-dotfiles.sh
@@ -1,10 +1,20 @@
-if [[ -e /etc/os-release ]]
+if [ "$ZSH_NAME" != "" ]
 then
-    source /etc/os-release
-    if [ ${ID} = "ubuntu" -o ${ID} = "debian" ]
+    echo "You appear to be running zsh."
+    echo "The dotfiles shell scripts are all written assuming bash."
+    echo "On macOS, you can change the default shell of the current"
+    echo "user account to bash using this command:"
+    echo ""
+    echo "    chsh -s /bin/bash"
+else
+    if [[ -e /etc/os-release ]]
     then
-        sudo apt-get --yes install curl
+        source /etc/os-release
+        if [ ${ID} = "ubuntu" -o ${ID} = "debian" ]
+        then
+            sudo apt-get --yes install curl
+        fi
     fi
+    export github_user=jafingerhut
+    bash -c "$(curl -fsSL https://raw.github.com/$github_user/dotfiles/master/bin/dotfiles)" && source ~/.bashrc
 fi
-export github_user=jafingerhut
-bash -c "$(curl -fsSL https://raw.github.com/$github_user/dotfiles/master/bin/dotfiles)" && source ~/.bashrc
