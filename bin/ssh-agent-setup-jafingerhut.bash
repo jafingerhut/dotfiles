c43a947db4460b8e8336755526ff830407a468f6
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-03-16 23:31:21 -0400

    Change name of SSH key file to use for ssh-agent This will make it easier for me to keep work vs. personal SSH key files distinct.

--- bin/ssh-agent-setup-jafingerhut.bash
+++ bin/ssh-agent-setup-jafingerhut.bash
@@ -4,4 +4,4 @@ echo 'git config --global user.email "andy_fingerhut@alum.wustl.edu"' >> ~/sourc
 echo 'git config --global credential.helper cache' >> ~/source-jafingerhut
 echo 'echo "Updated ~/.gitconfig for user id jafingerhut"' >> ~/source-jafingerhut
 source ~/source-jafingerhut
-ssh-add ~/.ssh/id_ed25519
+ssh-add ~/.ssh/idpersonal_ed25519
