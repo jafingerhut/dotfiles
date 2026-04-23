c2e8a23a841f5aa163440104ac1068283ed68289
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2026-04-23 14:18:45 -0400

    Use gnudd if present, only using dd if gnudd does not exist.
    Signed-off-by: Andy Fingerhut <andy_fingerhut@alum.wustl.edu>

--- bin/zero-empty-disk-space.sh
+++ bin/zero-empty-disk-space.sh
@@ -1,6 +1,21 @@
 #! /bin/bash
 
 set -x
+
+# On Ubuntu 26.04, the default installation of `dd` is a newer
+# Rust-based implementation.  In my testing, it is an order of
+# magnitude slower than the GNU version of `dd`.  Find and use GNU
+# `dd`, which is installed by default on Ubuntu 26.04 systems with the
+# name `gnudd`, if present.
+
+DD="dd"
+which gnudd >& /dev/null
+exit_status=$?
+if [ ${exit_status} -eq 0 ]
+then
+    DD="gnudd"
+fi
+
 FNAME="/bigemptyfile"
-sudo dd if=/dev/zero | sudo dd of=${FNAME} bs=4096k
+sudo ${DD} if=/dev/zero | sudo ${DD} of=${FNAME} bs=4096k
 sudo /bin/rm -f ${FNAME}
